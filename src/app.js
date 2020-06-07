cf2fb3b2b69817f044d64367257a83d77c94d02a
Nirav Boghra <niravboghra@Niravs-MacBook-Air.local>
2020-06-07 16:18:24 +1200

    indecision first commit

---
+++ src/app.js
@@ -0,0 +1,7 @@
+console.log('App.js file is running.')
+
+// JSX- JavaScript XML
+var template= <div><h1>Indecision App</h1><p>This my P!</p></div>;
+var appRoot=document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
